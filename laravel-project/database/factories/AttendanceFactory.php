99366139ae3c5da4357cb2171ae32e5e7060e126
Kazuhiro Sato <sato@gp1000.jp>
2024-04-23 16:28:37 +0900

    AttendanceFactoryの作成

---
+++ laravel-project/database/factories/AttendanceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Attendance;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Attendance>
+ */
+class AttendanceFactory extends Factory
+{
+    protected $model = Attendance::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => function () {
+                return User::factory()->create()->id;
+            },
+            'working_day' => today()->format('Y-m-d'),
+            'start_time' => Carbon::createFromTimeString('13:00:00'),
+            'finish_time' => Carbon::createFromTimeString('18:00:00'),
+        ];
+    }
+}
