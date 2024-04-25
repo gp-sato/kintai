f9b681ce82df49a7ca8b51574365cc079a692553
Kazuhiro Sato <sato@gp1000.jp>
2024-04-25 17:28:55 +0900

    seederの改造_AttendanceSeeder

--- laravel-project/database/seeders/AttendanceSeeder.php
+++ laravel-project/database/seeders/AttendanceSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Attendance;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Carbon\Carbon;
@@ -14,52 +15,43 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
-        // 今日、出勤した場合
-        Attendance::factory()->create([
-            'user_id' => 2,
-            'finish_time' => null,
-        ]);
+        $times = [
+            '13:30:00',
+            '14:00:00',
+            '14:30:00',
+            '15:00:00',
+            '15:30:00',
+            '16:00:00',
+            '16:30:00',
+            '17:00:00',
+            '17:30:00',
+        ];
 
-        // 昨日、出勤した場合
-        Attendance::factory()->create([
-            'user_id' => 2,
-            'working_day' => Carbon::yesterday()->format('Y-m-d'),
-        ]);
+        $users = User::where('is_admin', 0)->get();
+        $today = Carbon::today();
 
-        // 今日、遅刻した場合
-        Attendance::factory()->create([
-            'user_id' => 3,
-            'start_time' => Carbon::createFromTimeString('13:30:00'),
-            'finish_time' => null,
-        ]);
+        foreach ($users as $user) {
+            $day = Carbon::today()->addYears(-1);
+            while ($day->diffInDays($today) > 0) {
+                $i = rand(0, 10);
+                if ($i <= 7):
+                    Attendance::factory()->for($user)->create([
+                        'working_day' => $day->copy()->format('Y-m-d'),
+                    ]);
+                elseif ($i === 8):
+                    Attendance::factory()->for($user)->create([
+                        'working_day' => $day->copy()->format('Y-m-d'),
+                        'start_time' => Carbon::createFromTimeString($times[array_rand($times, 1)]),
+                    ]);
+                elseif ($i === 9):
+                    Attendance::factory()->for($user)->create([
+                        'working_day' => $day->copy()->format('Y-m-d'),
+                        'finish_time' => Carbon::createFromTimeString($times[array_rand($times, 1)]),
+                    ]);
+                endif;
 
-        Attendance::factory()->create([
-            'user_id' => 3,
-            'working_day' => Carbon::yesterday()->format('Y-m-d'),
-        ]);
-
-        Attendance::factory()->create([
-            'user_id' => 4,
-            'finish_time' => null,
-        ]);
-
-        // 昨日、早退した場合
-        Attendance::factory()->create([
-            'user_id' => 4,
-            'working_day' => Carbon::yesterday()->format('Y-m-d'),
-            'finish_time' => Carbon::createFromTimeString('15:30:00'),
-        ]);
-
-        // 昨日、欠勤した人
-        Attendance::factory()->create([
-            'user_id' => 5,
-            'finish_time' => null,
-        ]);
-
-        // 今日、まだ出勤していない人
-        Attendance::factory()->create([
-            'user_id' => 6,
-            'working_day' => Carbon::yesterday()->format('Y-m-d'),
-        ]);
+                $day->addDays(1);
+            }
+        }
     }
 }
