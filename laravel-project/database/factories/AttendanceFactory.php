9500b31812773b1e8ce51606f2cdec626dcc1123
Kazuhiro Sato <sato@gp1000.jp>
2024-05-02 14:24:24 +0900

    ランダムに時間を生成するメソッドの作成_AttendanceFactory

--- laravel-project/database/factories/AttendanceFactory.php
+++ laravel-project/database/factories/AttendanceFactory.php
@@ -28,4 +28,43 @@ class AttendanceFactory extends Factory
             'finish_time' => Carbon::createFromTimeString('18:00:00'),
         ];
     }
+
+    /**
+     * 出退勤時間を生成してstateで返すメソッド
+     *
+     * @param Carbon $working_day
+     * @return \Illuminate\Database\Eloquent\Factories\Factory
+     */
+    public function generateRandomTimesForDate($working_day)
+    {
+        $hours = [
+            1300,
+            1330,
+            1400,
+            1430,
+            1500,
+            1530,
+            1600,
+            1630,
+            1700,
+            1730,
+            1800,
+        ];
+
+        $hourKeys = array_rand($hours, 2);
+
+        if ($hours[$hourKeys[0]] < $hours[$hourKeys[1]]) {
+            $start = $hours[$hourKeys[0]];
+            $finish = $hours[$hourKeys[1]];
+        } else {
+            $start = $hours[$hourKeys[1]];
+            $finish = $hours[$hourKeys[0]];
+        }
+
+        return $this->state([
+            'working_day' => $working_day,
+            'start_time' => Carbon::parse($working_day)->setTime($start / 100, $start % 100, 00),
+            'finish_time' => Carbon::parse($working_day)->setTime($finish / 100, $finish % 100, 00),
+        ]);
+    }
 }
