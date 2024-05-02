373e7c5d4b97f79dfb41defc0ed2b63faf00ca78
Kazuhiro Sato <sato@gp1000.jp>
2024-05-02 14:25:52 +0900

    出退勤時間をメソッドで生成する_AttendanceSeeder

--- laravel-project/database/seeders/AttendanceSeeder.php
+++ laravel-project/database/seeders/AttendanceSeeder.php
@@ -15,38 +15,16 @@ class AttendanceSeeder extends Seeder
      */
     public function run(): void
     {
-        $hours = [13, 14, 15, 16, 17];
-        $minutes = [00, 30];
-
         $users = User::where('is_admin', 0)->get();
         $today = Carbon::today();
 
         foreach ($users as $user) {
             $day = Carbon::today()->addYears(-1);
             while ($day->diffInDays($today) > 0) {
-                $i = rand(0, 10);
-                if ($i <= 7):
-                    Attendance::factory()->for($user)->create([
-                        'working_day' => $day->copy()->format('Y-m-d'),
-                        'start_time' => Carbon::parse($day)->setTime(13, 00, 00),
-                        'finish_time' => Carbon::parse($day)->setTime(18, 00, 00),
-                    ]);
-                elseif ($i === 8):
-                    Attendance::factory()->for($user)->create([
-                        'working_day' => $day->copy()->format('Y-m-d'),
-                        'start_time' => Carbon::parse($day)->setTime($hours[array_rand($hours, 1)], $minutes[array_rand($minutes, 1)], 00),
-                        'finish_time' => Carbon::parse($day)->setTime(18, 00, 00),
-                    ]);
-                elseif ($i === 9):
-                    $hour = $hours[array_rand($hours, 1)];
-                    $minute = $hour === 13 ? 30 : $minutes[array_rand($minutes, 1)];
-                    Attendance::factory()->for($user)->create([
-                        'working_day' => $day->copy()->format('Y-m-d'),
-                        'start_time' => Carbon::parse($day)->setTime(13, 00, 00),
-                        'finish_time' => Carbon::parse($day)->setTime($hour, $minute, 00),
-                    ]);
-                endif;
-
+                $i = rand(0, 5);
+                if ($i !== 5) {
+                    Attendance::factory()->for($user)->generateRandomTimesForDate($day)->create();
+                }
                 $day->addDays(1);
             }
         }
