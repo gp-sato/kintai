8f145494e5d2fe1bec77eb73bd7db945ddd73a14
Kazuhiro Sato <sato@gp1000.jp>
2024-06-05 15:20:10 +0900

    トップタイトル中央寄せ

--- laravel-project/resources/views/top.blade.php
+++ laravel-project/resources/views/top.blade.php
@@ -34,7 +34,7 @@
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
 
                 <div class="mt-16">
-                    <span>GiftPlace 勤怠管理システム</span>
+                    <div class="text-center">GiftPlace 勤怠管理システム</div>
                 </div>
 
                 <div class="flex justify-center mt-16 px-0 sm:items-center sm:justify-between">
