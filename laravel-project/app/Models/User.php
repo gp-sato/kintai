77386a7d80c02d07bc406e2230e5e8211f85a6c5
Kazuhiro Sato <sato@gp1000.jp>
2024-06-12 16:39:19 +0900

    route()でurlを取得するように変更

--- laravel-project/app/Models/User.php
+++ laravel-project/app/Models/User.php
@@ -53,7 +53,7 @@ class User extends Authenticatable
      */
     public function sendPasswordResetNotification($token)
     {
-        $url = url("reset-password/${token}");
+        $url = route('password.reset', [$token]);
         $this->notify(new ResetPasswordNotificationJP($url));
     }
 }
