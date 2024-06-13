aeec21b41b64899b28182c0986aa2437f5d50978
Kazuhiro Sato <sato@gp1000.jp>
2024-06-13 17:24:46 +0900

    打刻ボタン作成

--- laravel-project/resources/views/stamping.blade.php
+++ laravel-project/resources/views/stamping.blade.php
@@ -1,7 +1,13 @@
 <x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-          {{ __('勤怠管理 - 打刻(stamping)') }}
-      </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('勤怠管理 - 打刻(stamping)') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-6 text-center">
+        <a href="#">
+            <button class="p-4 text-xl bg-white rounded-full">打刻(stamping)</button>
+        </a>
+    </div>
 </x-app-layout>
