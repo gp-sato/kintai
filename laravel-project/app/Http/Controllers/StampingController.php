2807487b77eaaa4340d48e2c801eba58491d7912
Kazuhiro Sato <sato@gp1000.jp>
2024-06-24 15:23:01 +0900

    打刻時間を現在時刻に変更

--- laravel-project/app/Http/Controllers/StampingController.php
+++ laravel-project/app/Http/Controllers/StampingController.php
@@ -41,32 +41,11 @@ class StampingController extends Controller
         }
 
         if (is_null($attendance->start_time)) {
-            $current_time = now();
-
-            if ($current_time->minute >= 0 && $current_time->minute <= 29):
-                $start_time = Carbon::createFromTime($current_time->hour, 30, 0);
-            elseif ($current_time->minute >= 30 && $current_time->minute <= 59):
-                $start_time = Carbon::createFromTime($current_time->hour + 1, 0, 0);
-            endif;
-
-            $attendance->start_time = $start_time;
+            $attendance->start_time = now();
             $attendance->finish_time = null;
             $attendance->save();
-
         } elseif (is_null($attendance->finish_time)) {
-            $current_time = now();
-
-            if ($current_time->minute >= 0 && $current_time->minute <= 29):
-                $finish_time = Carbon::createFromTime($current_time->hour, 0, 0);
-            elseif ($current_time->minute >= 30 && $current_time->minute <= 59):
-                $finish_time = Carbon::createFromTime($current_time->hour, 30, 0);
-            endif;
-
-            if ($finish_time->lt($attendance->start_time)) {
-                $finish_time = $attendance->start_time;
-            }
-
-            $attendance->finish_time = $finish_time;
+            $attendance->finish_time = now();
             $attendance->save();
         }
 
