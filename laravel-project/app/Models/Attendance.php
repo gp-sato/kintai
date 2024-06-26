bfaf70bd363d62c7e60b650c0ac23798f5d565cb
Kazuhiro Sato <sato@gp1000.jp>
2024-06-26 14:18:47 +0900

    Attendaceモデルからメソッドを削除

--- laravel-project/app/Models/Attendance.php
+++ laravel-project/app/Models/Attendance.php
@@ -25,30 +25,4 @@ class Attendance extends Model
     public function user() {
         return $this->belongsTo(user::class);
     }
-
-    /**
-     * 打刻丸め
-     * 45～14分→0分
-     * 15～44分→30分
-     *
-     * @param Carbon $stampTime
-     * @return Carbon $roundTime | null
-     */
-    public static function stampRounding(?Carbon $stampTime)
-    {
-        if (!is_null($stampTime)) {
-            if ($stampTime->minute >= 45) {
-                $buff = $stampTime->addHours(1);
-                $roundTime = $buff->minute(0);
-            } elseif ($stampTime->minute < 15) {
-                $roundTime = $stampTime->minute(0);
-            } else {
-                $roundTime = $stampTime->minute(30);
-            }
-
-            return $roundTime;
-        }
-
-        return null;
-    }
 }
