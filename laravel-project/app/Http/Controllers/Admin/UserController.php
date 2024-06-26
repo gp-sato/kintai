356232b54495234411f687504957adce8ad01088
Kazuhiro Sato <sato@gp1000.jp>
2024-06-26 16:27:47 +0900

    一般ユーザーの管理者画面へのアクセス禁止

--- laravel-project/app/Http/Controllers/Admin/UserController.php
+++ laravel-project/app/Http/Controllers/Admin/UserController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class UserController extends Controller
 {
     public function index()
     {
+        if (Gate::denies('admin.authority')) {
+            abort(403);
+        }
+
         return view('admin.index');
     }
 }
