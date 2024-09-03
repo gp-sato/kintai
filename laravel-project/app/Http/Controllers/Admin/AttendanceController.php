b389d278b9da8c3d652ba3a13cfee344f8fc817e
Kazuhiro Sato <sato@gp1000.jp>
2024-09-03 17:12:04 +0900

    バリデーション作成

--- laravel-project/app/Http/Controllers/Admin/AttendanceController.php
+++ laravel-project/app/Http/Controllers/Admin/AttendanceController.php
@@ -8,6 +8,7 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Validator;
 
 class AttendanceController extends Controller
 {
@@ -72,7 +73,7 @@ class AttendanceController extends Controller
         return view('admin.attendance.edit', compact(['user', 'date', 'attendance']));
     }
 
-    public function update(User $user, $date)
+    public function update(Request $request, User $user, $date)
     {
         if (Gate::denies('admin.authority')) {
             abort(403);
@@ -111,7 +112,23 @@ class AttendanceController extends Controller
             abort(404);
         }
 
+        $hours = '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23';
+        $minutes = '0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59';
 
+        $validator = Validator::make($request->all(), [
+            'start_hour' => "required|integer|in:{$hours}",
+            'start_minute' => "required|integer|in:{$minutes}",
+            'finish_hour' => "required|integer|in:{$hours}|gte:start_hour",
+            'finish_minute' => "required|integer|in:{$minutes}",
+        ]);
+        $validator->sometimes('finish_minute', "gte:start_minute", function ($request) {
+            return $request->start_hour == $request->finish_hour;
+        });
+        if ($validator->fails()) {
+            return redirect()->route('admin.attendance.edit', ['user' => $user, 'date' => $date]);
+        }
+
+        $formData = $validator->validated();
 
         session()->forget('user_id');
         session()->forget('working_day');
