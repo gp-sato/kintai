d63810d8b88339271812843934ff29b0db623bc2
Kazuhiro Sato <sato@gp1000.jp>
2024-09-18 14:34:49 +0900

    打刻丸めのアクセサ定義

--- laravel-project/app/Models/Attendance.php
+++ laravel-project/app/Models/Attendance.php
@@ -25,4 +25,41 @@ class Attendance extends Model
     public function user() {
         return $this->belongsTo(user::class);
     }
+
+    /**
+     * 打刻丸め
+     * 45～14分→0分
+     * 15～44分→30分
+     *
+     * @param Carbon $stampTime
+     * @return Carbon $roundTime | null
+     */
+    protected static function timeRounding(?Carbon $stampTime)
+    {
+      if (is_null($stampTime)) return null;
+
+      if ($stampTime->minute >= 45) {
+          $roundTime = $stampTime->addHours(1)->minute(0)->second(0);
+      } elseif ($stampTime->minute < 15) {
+          $roundTime = $stampTime->minute(0)->second(0);
+      } else {
+          $roundTime = $stampTime->minute(30)->second(0);
+      }
+      return $roundTime;
+    }
+
+    public function getRoundStartTimeAttribute()
+    {
+        return self::timeRounding($this->start_time);
+    }
+
+    public function getRoundFinishTimeAttribute()
+    {
+        return self::timeRounding($this->finish_time);
+    }
+
+    public function getWorkingTimeAttribute()
+    {
+        return !is_null($this->roundFinishTime) ? $this->roundStartTime->diffInMinutes($this->roundFinishTime) : null;
+    }
 }
