9ac3b4709caff4cd72c2118ffab51ac0427e66a6
Kazuhiro Sato <sato@gp1000.jp>
2024-10-10 16:51:41 +0900

    不要なuse文の削除

--- laravel-project/app/Http/Controllers/Admin/AttendanceController.php
+++ laravel-project/app/Http/Controllers/Admin/AttendanceController.php
@@ -10,7 +10,6 @@ use App\Models\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Validator;
 
 class AttendanceController extends Controller
 {
