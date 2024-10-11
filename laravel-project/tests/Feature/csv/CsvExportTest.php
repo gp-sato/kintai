5ed93311db63d915cff43741320e859691ce4822
Kazuhiro Sato <sato@gp1000.jp>
2024-10-11 16:53:31 +0900

    ダウンロードされたCSVの内容もアサートする

--- laravel-project/tests/Feature/csv/CsvExportTest.php
+++ laravel-project/tests/Feature/csv/CsvExportTest.php
@@ -26,11 +26,26 @@ class CsvExportTest extends TestCase
 
         $this->user = User::factory()->state(['is_admin' => 0])->create();
 
-        foreach (range(1, 10) as $d) {
-            $date = Carbon::create(2024, 4, $d);
-            Attendance::factory()->for($this->user)->generateRandomTimesForDate($date)->create();
-        }
+        $attendance = new Attendance();
+        $attendance->user_id = $this->user->id;
+        $attendance->working_day = '2024-04-01';
+        $attendance->start_time = Carbon::create(2024, 4, 1, 13);
+        $attendance->finish_time = Carbon::create(2024, 4, 1, 18);
+        $attendance->save();
 
+        $attendance = new Attendance();
+        $attendance->user_id = $this->user->id;
+        $attendance->working_day = '2024-04-02';
+        $attendance->start_time = Carbon::create(2024, 4, 2, 13, 25);
+        $attendance->finish_time = Carbon::create(2024, 4, 2, 18, 10);
+        $attendance->save();
+
+        $attendance = new Attendance();
+        $attendance->user_id = $this->user->id;
+        $attendance->working_day = '2024-04-04';
+        $attendance->start_time = Carbon::create(2024, 4, 4, 12, 50);
+        $attendance->finish_time = Carbon::create(2024, 4, 4, 17, 40);
+        $attendance->save();
     }
 
     public function tearDown(): void
@@ -79,5 +94,13 @@ class CsvExportTest extends TestCase
 
         $filename = "2024年4月分職員勤務実績記録票（" . $this->user->name . "）.csv";
         $response->assertDownload($filename);
+
+        $csvContent = $response->getContent();
+        $expectedContent = "2024,4,\"{$this->user->name}\",14:00\r\n日付,出勤時間,退勤時間,勤務時間\r\n1,13:00,18:00,05:00\r\n2,13:30,18:00,04:30\r\n3,,,\r\n4,13:00,17:30,04:30\r\n";
+        foreach (range(5, 31) as $i) {
+            $expectedContent .= "{$i},,,\r\n";
+        }
+        $expectedContent = mb_convert_encoding($expectedContent, 'SJIS-win', 'UTF-8');
+        $this->assertEquals($expectedContent, $csvContent);
     }
 }
