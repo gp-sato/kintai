da74aa721e8f9287263b5a42e18f7960f66de6f2
Kazuhiro Sato <sato@gp1000.jp>
2024-10-21 17:25:15 +0900

    パスワードの但し書き追加

--- laravel-project/resources/views/admin/user/create.blade.php
+++ laravel-project/resources/views/admin/user/create.blade.php
@@ -22,7 +22,7 @@
       </div>
 
       <div class="mt-4">
-        <x-input-label for="password" :value="__('パスワード')" />
+        <x-input-label for="password" :value="__('パスワード（8文字以上半角英数）')" />
         <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required />
         <button id="show-password-button" class="mt-1">表示</button>
         <x-input-error :messages="$errors->get('password')" class="mt-2" />
