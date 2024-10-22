2cf2edc5227c12e74ab5420138fb8f23e6fd2011
Kazuhiro Sato <sato@gp1000.jp>
2024-10-22 15:23:23 +0900

    パスワードリセットのパスワード欄の伏せ字に表示・非表示対応

--- laravel-project/resources/views/auth/reset-password.blade.php
+++ laravel-project/resources/views/auth/reset-password.blade.php
@@ -13,19 +13,24 @@
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
+        <div class="mt-4 password-wrap">
             <x-input-label for="password" :value="__('Password（新しいパスワード）')" />
             <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
+            <img id="password-eye-slash-solid" class="eye-slash-solid" src="{{ asset('images/eye-slash-solid.svg') }}">
+            <img id="password-eye-solid" class="eye-solid" src="{{ asset('images/eye-solid.svg') }}" style="display: none">
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password（確認）')" />
+        <div class="mt-4 password-confirm-wrap">
+            <x-input-label for="password-confirm" :value="__('Confirm Password（確認）')" />
 
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
+            <x-text-input id="password-confirm" class="block mt-1 w-full"
                                 type="password"
                                 name="password_confirmation" required autocomplete="new-password" />
+
+            <img id="password-confirm-eye-slash-solid" class="eye-slash-solid" src="{{ asset('images/eye-slash-solid.svg') }}">
+            <img id="password-confirm-eye-solid" class="eye-solid" src="{{ asset('images/eye-solid.svg') }}" style="display: none">
 
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
