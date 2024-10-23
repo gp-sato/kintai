7d1c98178fb1143494977751ac68cb8622331e4b
Kazuhiro Sato <sato@gp1000.jp>
2024-10-23 15:22:04 +0900

    CSS_クラスをiconにまとめる

--- laravel-project/resources/views/components/password-input.blade.php
+++ laravel-project/resources/views/components/password-input.blade.php
@@ -1,7 +1,7 @@
 <div class="password-wrap">
   {{ $slot }}
   <div class="eye-icons-wrap">
-    <img class="eye-slash-solid icon show" src="{{ asset('images/eye-slash-solid.svg') }}">
-    <img class="eye-solid icon" src="{{ asset('images/eye-solid.svg') }}">
+    <img class="icon show" src="{{ asset('images/eye-slash-solid.svg') }}">
+    <img class="icon" src="{{ asset('images/eye-solid.svg') }}">
   </div>
 </div>
